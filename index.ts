053b7bf9afb4a24e14b9ee9887e477900325e947
Eduardo Casanova <53999059+ed3899@users.noreply.github.com>
2024-03-29 23:11:40 +0000

    stuff

--- index.ts
+++ index.ts
@@ -2,8 +2,36 @@ import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 import * as awsx from "@pulumi/awsx";
 
-// Create an AWS resource (S3 Bucket)
-const bucket = new aws.s3.Bucket("my-bucket");
+const config = new pulumi.Config();
+const currentEnvName = config.require("currentEnvironmentName")
+const orgLogin = config.require("orgLogin")
+const lb = new awsx.lb.ApplicationLoadBalancer("lb");
+const cluster = new aws.ecs.Cluster("cluster");
 
-// Export the name of the bucket
-export const bucketName = bucket.id;
+const repo = new awsx.ecr.Repository(`${currentEnvName}-${orgLogin}-ecr_repository`, {
+  forceDelete: true,
+});
+
+const service = new awsx.ecs.FargateService("service", {
+  cluster: cluster.arn,
+  assignPublicIp: true,
+  desiredCount: 2,
+  taskDefinitionArgs: {
+    container: {
+      name: "my-service",
+      image: "nginx:latest",
+
+      cpu: 128,
+      memory: 512,
+      essential: true,
+      portMappings: [
+        {
+          containerPort: 80,
+          targetGroup: lb.defaultTargetGroup,
+        },
+      ],
+    },
+  },
+});
+
+export const url = pulumi.interpolate`http://${lb.loadBalancer.dnsName}`;
